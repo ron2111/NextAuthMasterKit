c5e0ffe7466a315f5d6db1496fc40ebb4bc793e9
Rohan Sharma <onlyrohan21@gmail.com>
2024-05-05 00:37:42 +0530

    register functionality and next auth init

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,34 @@
-"use server"
+"use server";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
-import {RegisterSchema} from "@/schemas"
+import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
-export const register = async (values: z.infer<typeof RegisterSchema>)=> {
-    const validatedFields = RegisterSchema.safeParse(values)
-     if(!validatedFields.success) {
-         return {error: "Invalid fields!"}
-     }
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
 
-    return {success: "Email Sent!"}
-}
+  const { email, password, name } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10); // 10 salt rounds
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: Send verification token email
+  return { success: "User created!" };
+};
